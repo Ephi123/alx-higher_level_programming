1f5e9d0fa9be0466793f65e9f1059bcc2366e889
Ephi123 <99554853+Ephi123@users.noreply.github.com>
2022-12-06 01:22:49 +0300

    last fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_str = ""
-    for c in my_string:
-        if c is not 'c' and i is not 'C':
-            new_str += c
+    for i in my_string:
+        if i is not 'c' and i is not 'C':
+            new_str += i
     return (new_str)
