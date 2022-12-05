72e107a696620c6cff67a4d919886eae8c15a59f
Ephi123 <99554853+Ephi123@users.noreply.github.com>
2022-12-06 01:18:40 +0300

    hot-fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-   return (my_string.translate({ord('c'): None for c in "cC"}))
+  new_str = " "
+  for c in my_string:
+     if c != 'C' and c != 'c':
+       new_str += c 
+  return new_str
